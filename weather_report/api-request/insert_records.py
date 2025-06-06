ae2263a28b3b45ead7e0f8798b9c2741ecfc514a
talha915 <talhazafa915426@gmail.com>
2025-06-06 22:37:20 +0200

    db connection established

--- weather_report/api-request/insert_records.py
+++ weather_report/api-request/insert_records.py
@@ -1,3 +1,19 @@
+import psycopg2
 from api_request import mock_fetch_data
 
-print(mock_fetch_data())
+def connect_to_db():
+    try:
+        print(f"🔄 Connecting to the postgresql database.....")
+        conn = psycopg2.connect(
+            host="localhost",
+            port="5000",
+            dbname="db",
+            user="db_user",
+            password="db_password"
+        )
+        print(f"✅ Connection established successfully!")
+    except Exception as e:
+        print(f"❌ Error while connecting to database: {e}")    
+
+
+connect_to_db()        
