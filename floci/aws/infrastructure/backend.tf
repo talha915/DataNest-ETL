cccc5082164b46b8842a323e9af7938af47509f1
talha915 <talhazafar915426@gmail.com>
2026-09-26 20:37:12 +0200

    updates

--- floci/aws/infrastructure/backend.tf
+++ floci/aws/infrastructure/backend.tf
@@ -12,7 +12,7 @@ terraform {
     }
 
     skip_credentials_validation = true
-    skip_region_validation      = true
+    skip_region_validation = true
     skip_requesting_account_id  = true
 
     use_path_style = true
